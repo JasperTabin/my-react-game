4124ad90d02af7b4e332595cde55d313197e8d3f
Your Name <your.email@example.com>
2023-12-31 17:06:34 +0800

    Changes

--- src/Maze.js
+++ src/Maze.js
@@ -103,7 +103,7 @@ const levels = [
       { row: 13, col: 12 },
       { row: 14, col: 3 },
     ],
-    collectibleQuantity: 16,
+    collectibleQuantity: 1,
   },
   // Level 3
   {
@@ -112,10 +112,18 @@ const levels = [
       { row: 1, col: 4 },
       { row: 1, col: 7 },
       { row: 1, col: 12 },
+      { row: 1, col: 18 },
+      { row: 1, col: 22 },
+      { row: 1, col: 26 },
       { row: 2, col: 7 },
       { row: 2, col: 8 },
       { row: 2, col: 9 },
       { row: 2, col: 14 },
+      { row: 2, col: 15 },
+      { row: 2, col: 16 },
+      { row: 2, col: 20 },
+      { row: 2, col: 24 },
+      { row: 2, col: 28 },
       { row: 2, col: 11 },
       { row: 2, col: 12 },
       { row: 2, col: 1 },
@@ -123,21 +131,59 @@ const levels = [
       { row: 2, col: 4 },
       { row: 2, col: 5 },
       { row: 3, col: 4 },
+      { row: 3, col: 16 },
+      { row: 3, col: 17 },
+      { row: 3, col: 18 },
+      { row: 3, col: 19 },
+      { row: 3, col: 20 },
+      { row: 3, col: 21 },
+      { row: 3, col: 22 },
+      { row: 3, col: 23 },
+      { row: 3, col: 24 },
+      { row: 3, col: 25 },
+      { row: 3, col: 26 },
+      { row: 3, col: 27 },
+      { row: 3, col: 28 },
+      { row: 3, col: 29 },
       { row: 4, col: 4 },
       { row: 4, col: 8 },
       { row: 4, col: 10 },
       { row: 4, col: 12 },
       { row: 4, col: 13 },
       { row: 4, col: 14 },
+      { row: 4, col: 14 },
+      { row: 4, col: 16 },
       { row: 4, col: 6 },
       { row: 4, col: 3 },
       { row: 4, col: 2 },
       { row: 4, col: 7 },
+      { row: 4, col: 24 },
+      { row: 4, col: 26 },
+      { row: 4, col: 28 },
       { row: 5, col: 2 },
       { row: 5, col: 7 },
       { row: 5, col: 10 },
       { row: 5, col: 12 },
+      { row: 5, col: 18 },
+      { row: 5, col: 20 },
+      { row: 5, col: 22 },
       { row: 6, col: 14 },
+      { row: 6, col: 15 },
+      { row: 6, col: 16 },
+      { row: 6, col: 17 },
+      { row: 6, col: 18 },
+      { row: 6, col: 19 },
+      { row: 6, col: 20 },
+      { row: 6, col: 21 },
+      { row: 6, col: 22 },
+      { row: 6, col: 23 },
+      { row: 6, col: 24 },
+      { row: 6, col: 25 },
+      { row: 6, col: 26 },
+      { row: 6, col: 27 },
+      { row: 6, col: 28 },
+      { row: 6, col: 29 },
+
       { row: 6, col: 4 },
       { row: 6, col: 10 },
       { row: 7, col: 1 },
@@ -152,6 +198,20 @@ const levels = [
       { row: 8, col: 9 },
       { row: 8, col: 12 },
       { row: 8, col: 13 },
+      { row: 8, col: 15 },
+      { row: 8, col: 18 },
+      { row: 8, col: 19 },
+      { row: 8, col: 20 },
+      { row: 8, col: 21 },
+      { row: 8, col: 22 },
+      { row: 8, col: 23 },
+      { row: 8, col: 24 },
+      { row: 8, col: 25 },
+      { row: 8, col: 26 },
+      { row: 8, col: 27 },
+      { row: 8, col: 28 },
+      { row: 8, col: 29 },
+      { row: 8, col: 16 },
       { row: 9, col: 12 },
       { row: 9, col: 1 },
       { row: 9, col: 3 },
@@ -160,10 +220,25 @@ const levels = [
       { row: 9, col: 7 },
       { row: 9, col: 9 },
       { row: 9, col: 11 },
+      { row: 9, col: 16 },
       { row: 10, col: 3 },
       { row: 10, col: 6 },
       { row: 10, col: 9 },
       { row: 10, col: 14 },
+      { row: 10, col: 15 },
+      { row: 10, col: 16 },
+      { row: 10, col: 17 },
+      { row: 10, col: 19 },
+      { row: 10, col: 20 },
+      { row: 10, col: 21 },
+      { row: 10, col: 22 },
+      { row: 10, col: 23 },
+      { row: 10, col: 24 },
+      { row: 10, col: 25 },
+      { row: 10, col: 26 },
+      { row: 10, col: 27 },
+      { row: 10, col: 28 },
+      { row: 10, col: 29 },
       { row: 11, col: 3 },
       { row: 11, col: 2 },
       { row: 11, col: 4 },
@@ -173,10 +248,18 @@ const levels = [
       { row: 11, col: 8 },
       { row: 11, col: 10 },
       { row: 11, col: 12 },
+      { row: 11, col: 17 },
+      { row: 11, col: 19 },
+      { row: 11, col: 23 },
+      { row: 11, col: 27 },
       { row: 12, col: 5 },
       { row: 12, col: 10 },
       { row: 12, col: 14 },
       { row: 12, col: 12 },
+      { row: 12, col: 15 },
+      { row: 12, col: 21 },
+      { row: 12, col: 25 },
+      { row: 12, col: 29 },
       { row: 13, col: 1 },
       { row: 13, col: 3 },
       { row: 13, col: 5 },
@@ -184,31 +267,100 @@ const levels = [
       { row: 13, col: 8 },
       { row: 13, col: 10 },
       { row: 13, col: 12 },
+      { row: 13, col: 15 },
+      { row: 13, col: 17 },
+      { row: 13, col: 18 },
+      { row: 13, col: 19 },
+      { row: 13, col: 20 },
+      { row: 13, col: 21 },
+      { row: 13, col: 22 },
+      { row: 13, col: 23 },
+      { row: 13, col: 24 },
+      { row: 13, col: 25 },
+      { row: 13, col: 26 },
+      { row: 13, col: 27 },
+      { row: 13, col: 28 },
+      { row: 13, col: 29 },
       { row: 14, col: 3 },
+      { row: 14, col: 15 },
+      { row: 14, col: 29 },
       { row: 15, col: 6 },
       { row: 15, col: 7 },
       { row: 15, col: 9 },
       { row: 15, col: 10 },
+      { row: 15, col: 11 },
+      { row: 15, col: 12 },
+      { row: 15, col: 13 },
+      { row: 15, col: 15 },
+      { row: 15, col: 16 },
+      { row: 15, col: 17 },
+      { row: 15, col: 18 },
+      { row: 15, col: 19 },
+      { row: 15, col: 20 },
+      { row: 15, col: 21 },
+      { row: 15, col: 22 },
+      { row: 15, col: 23 },
+      { row: 15, col: 24 },
+      { row: 15, col: 25 },
+      { row: 15, col: 26 },
+      { row: 15, col: 27 },
+      { row: 15, col: 29 },
       { row: 16, col: 1 },
       { row: 16, col: 2 },
       { row: 16, col: 4 },
       { row: 16, col: 6 },
       { row: 16, col: 10 },
+      { row: 16, col: 12 },
+      { row: 16, col: 15 },
+      { row: 16, col: 29 },
       { row: 17, col: 4 },
       { row: 17, col: 5 },
       { row: 17, col: 6 },
       { row: 17, col: 7 },
       { row: 17, col: 8 },
       { row: 17, col: 10 },
+      { row: 17, col: 29 },
+      { row: 17, col: 15 },
+      { row: 17, col: 17 },
+      { row: 17, col: 18 },
+      { row: 17, col: 19 },
+      { row: 17, col: 20 },
+      { row: 17, col: 21 },
+      { row: 17, col: 22 },
+      { row: 17, col: 23 },
+      { row: 17, col: 24 },
+      { row: 17, col: 25 },
+      { row: 17, col: 26 },
+      { row: 17, col: 27 },
+      { row: 17, col: 28 },
       { row: 18, col: 4 },
       { row: 18, col: 3 },
       { row: 18, col: 2 },
       { row: 18, col: 6 },
       { row: 18, col: 10 },
+      { row: 18, col: 15 },
+      { row: 18, col: 14 },
+      { row: 18, col: 13 },
+      { row: 18, col: 12 },
+      { row: 18, col: 29 },
       { row: 19, col: 4 },
       { row: 19, col: 6 },
       { row: 19, col: 8 },
       { row: 19, col: 10 },
+      { row: 19, col: 15 },
+      { row: 19, col: 16 },
+      { row: 19, col: 17 },
+      { row: 19, col: 18 },
+      { row: 19, col: 19 },
+      { row: 19, col: 20 },
+      { row: 19, col: 21 },
+      { row: 19, col: 22 },
+      { row: 19, col: 23 },
+      { row: 19, col: 24 },
+      { row: 19, col: 25 },
+      { row: 19, col: 26 },
+      { row: 19, col: 27 },
+      { row: 19, col: 29 },
       { row: 20, col: 1 },
       { row: 20, col: 2 },
       { row: 20, col: 4 },
@@ -216,9 +368,22 @@ const levels = [
       { row: 20, col: 8 },
       { row: 20, col: 9 },
       { row: 20, col: 10 },
+      { row: 20, col: 11 },
+      { row: 20, col: 12 },
+      { row: 20, col: 13 },
+      { row: 20, col: 14 },
+      { row: 20, col: 15 },
+      { row: 20, col: 19 },
+      { row: 20, col: 23 },
+      { row: 20, col: 29 },
+      { row: 20, col: 27 },
       { row: 21, col: 4 },
       { row: 21, col: 6 },
       { row: 21, col: 10 },
+      { row: 21, col: 17 },
+      { row: 21, col: 21 },
+      { row: 21, col: 25 },
+      { row: 21, col: 29 },
       { row: 22, col: 4 },
       { row: 22, col: 6 },
       { row: 22, col: 3 },
@@ -229,7 +394,18 @@ const levels = [
       { row: 22, col: 13 },
       { row: 22, col: 14 },
       { row: 22, col: 15 },
+      { row: 22, col: 17 },
+      { row: 22, col: 18 },
+      { row: 22, col: 20 },
+      { row: 22, col: 21 },
       { row: 22, col: 16 },
+      { row: 22, col: 24 },
+      { row: 22, col: 22 },
+      { row: 22, col: 25 },
+      { row: 22, col: 26 },
+      { row: 22, col: 27 },
+      { row: 22, col: 28 },
+      { row: 22, col: 29 },
       { row: 23, col: 4 },
       { row: 23, col: 6 },
       { row: 23, col: 16 },
@@ -237,6 +413,8 @@ const levels = [
       { row: 23, col: 7 },
       { row: 23, col: 10 },
       { row: 23, col: 13 },
+      { row: 23, col: 21 },
+      { row: 23, col: 29 },
       { row: 24, col: 4 },
       { row: 24, col: 6 },
       { row: 24, col: 1 },
@@ -246,6 +424,14 @@ const levels = [
       { row: 24, col: 12 },
       { row: 24, col: 14 },
       { row: 24, col: 18 },
+      { row: 24, col: 21 },
+      { row: 24, col: 22 },
+      { row: 24, col: 23 },
+      { row: 24, col: 24 },
+      { row: 24, col: 25 },
+      { row: 24, col: 27 },
+      { row: 24, col: 28 },
+      { row: 24, col: 29 },
       { row: 25, col: 4 },
       { row: 25, col: 8 },
       { row: 25, col: 10 },
@@ -253,6 +439,9 @@ const levels = [
       { row: 25, col: 16 },
       { row: 25, col: 17 },
       { row: 25, col: 18 },
+      { row: 25, col: 19 },
+      { row: 25, col: 21 },
+      { row: 25, col: 29 },
       { row: 26, col: 3 },
       { row: 26, col: 2 },
       { row: 26, col: 4 },
@@ -263,11 +452,23 @@ const levels = [
       { row: 26, col: 14 },
       { row: 26, col: 10 },
       { row: 26, col: 17 },
+      { row: 26, col: 19 },
+      { row: 26, col: 21 },
+      { row: 26, col: 22 },
+      { row: 26, col: 24 },
+      { row: 26, col: 25 },
+      { row: 26, col: 26 },
+      { row: 26, col: 27 },
+      { row: 26, col: 28 },
+      { row: 26, col: 29 },
+      { row: 26, col: 1 },
+      { row: 26, col: 19 },
       { row: 27, col: 17 },
       { row: 27, col: 16 },
       { row: 27, col: 4 },
       { row: 27, col: 7 },
       { row: 27, col: 12 },
+      { row: 27, col: 29 },
       { row: 28, col: 4 },
       { row: 28, col: 1 },
       { row: 28, col: 2 },
@@ -278,16 +479,40 @@ const levels = [
       { row: 28, col: 11 },
       { row: 28, col: 12 },
       { row: 28, col: 13 },
+      { row: 28, col: 14 },
+      { row: 28, col: 16 },
+      { row: 28, col: 17 },
+      { row: 28, col: 18 },
+      { row: 28, col: 16 },
+      { row: 28, col: 19 },
+      { row: 28, col: 20 },
+      { row: 28, col: 21 },
+      { row: 28, col: 22 },
+      { row: 28, col: 24 },
+      { row: 28, col: 25 },
+      { row: 28, col: 26 },
+      { row: 28, col: 27 },
+      { row: 28, col: 28 },
+      { row: 28, col: 29 },
+      { row: 29, col: 17 },
       { row: 29, col: 4 },
       { row: 29, col: 5 },
       { row: 29, col: 6 },
       { row: 29, col: 7 },
       { row: 29, col: 9 },
+      { row: 29, col: 21 },
+      { row: 29, col: 25 },
+      { row: 29, col: 29 },
       { row: 29, col: 11 },
       { row: 29, col: 13 },
+      { row: 29, col: 13 },
       { row: 30, col: 2 },
+      { row: 30, col: 15 },
+      { row: 30, col: 19 },
+      { row: 30, col: 23 },
+      { row: 30, col: 27 },
     ],
-    collectibleQuantity: 32,
+    collectibleQuantity: 1,
   },
   // Level 4
   {
@@ -295,7 +520,7 @@ const levels = [
     wallPositions: [
       // ... wall positions for Level 4
     ],
-    collectibleQuantity: 12,
+    collectibleQuantity: 1,
   },
   // ... add more levels as needed
 ];
@@ -358,31 +583,16 @@ const Maze = ({ toggleMusic, musicPlaying, exitGame }) => {
     new Audio(collectibleSound)
   );
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  const [zoomLevel, setZoomLevel] = useState(1);
-
-  const handleResize = () => {
-    const mobileThreshold = 768; // Adjust this threshold as needed
-    const currentWidth = window.innerWidth;
-
-    if (currentWidth <= mobileThreshold && !isMobile) {
-      setIsMobile(true);
-      setZoomLevel(0.5); // Adjust the zoom level as needed
-    } else if (currentWidth > mobileThreshold && isMobile) {
-      setIsMobile(false);
-      setZoomLevel(1);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [handleResize]);
 
   const handlePauseResumeClick = () => {
     setPaused(!paused);
     setShowButtons(!paused);
+  };
+
+  const showInstructions = () => {
+    alert(
+      "Instructions:\nUse arrow keys or button to navigate through the maze."
+    );
   };
 
   useEffect(() => {
@@ -528,7 +738,6 @@ const Maze = ({ toggleMusic, musicPlaying, exitGame }) => {
 
           newMaze[newPlayerRow][newPlayerCol] = 3;
           setMaze(newMaze);
-
           setPlayerRow(newPlayerRow);
           setPlayerCol(newPlayerCol);
         }
@@ -616,12 +825,21 @@ const Maze = ({ toggleMusic, musicPlaying, exitGame }) => {
     };
 
     return (
-      <div className="maze">
-        <div className="info-bar">
-          <div className="collectibles">COLLECTIBLES: {collectibles}</div>
-          <div className="current-level">LEVEL: {currentLevelIndex + 1}</div>
+      <div className="maze-container">
+        <div className="icon-container">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/Pause.png`}
+            alt="Pause Icon"
+            className="pause-icon"
+            onClick={handlePauseResumeClick}
+          />
         </div>
-        <div className="game-board">
+        <div className="maze">
+          <div className="info-bar">
+            <div className="collectibles">COLLECTIBLES: {collectibles}</div>
+            <div className="current-level">LEVEL: {currentLevelIndex + 1}</div>
+          </div>
+
           {maze.map((row, rowIndex) => (
             <div key={rowIndex} className="maze-row">
               {row.map((cell, colIndex) => (
@@ -668,70 +886,58 @@ const Maze = ({ toggleMusic, musicPlaying, exitGame }) => {
               ))}
             </div>
           ))}
-        </div>
-        <div className="icon-container">
-          <img
-            src={`${process.env.PUBLIC_URL}Pause.png`}
-            alt="Pause Icon"
-            className="pause-icon"
-            onClick={handlePauseResumeClick}
-          />
-        </div>
-        {isMobile && (
-          <div className="Mobile-Controls">
-            <button
-              className="mobile-button"
-              onClick={() => handleButtonPress("up")}
-            >
-              Up
-            </button>
-            <button
-              className="mobile-button"
-              onClick={() => handleButtonPress("down")}
-            >
-              Down
-            </button>
-            <button
-              className="mobile-button"
-              onClick={() => handleButtonPress("left")}
-            >
-              Left
-            </button>
-            <button
-              className="mobile-button"
-              onClick={() => handleButtonPress("right")}
-            >
-              Rightt
-            </button>
-          </div>
-        )}
-        {showButtons && (
-          <div className="popup">
-            <p>CAT MAZE GAME</p>
-            <button onClick={handlePauseResumeClick}>Resume</button>
-            <button onClick={handleExitClick}>Exit Game</button>
-          </div>
-        )}
-        {gameWon && (
-          <div className="popup">
-            <div className="popup-content">
-              <p>Congratulations! You won!</p>
-              <button onClick={handleNextLevelClick}>Next Level</button>
-              <button onClick={handleTryAgainClick}>Try Again</button>
+
+          {isMobile && (
+            <div className="Mobile-Controls">
+              <button
+                className="mobile-button"
+                onClick={() => handleButtonPress("up")}
+              >
+                Up
+              </button>
+              <button
+                className="mobile-button"
+                onClick={() => handleButtonPress("down")}
+              >
+                Down
+              </button>
+              <button
+                className="mobile-button"
+                onClick={() => handleButtonPress("left")}
+              >
+                Left
+              </button>
+              <button
+                className="mobile-button"
+                onClick={() => handleButtonPress("right")}
+              >
+                Right
+              </button>
             </div>
-          </div>
-        )}
+          )}
+          {showButtons && (
+            <div className="popup">
+              <p>CAT MAZE GAME</p>
+              <button onClick={handlePauseResumeClick}>Resume</button>
+              <button onClick={showInstructions}>Instructions</button>
+              <button onClick={handleExitClick}>Exit Game</button>
+            </div>
+          )}
+          {gameWon && (
+            <div className="popup">
+              <div className="popup-content">
+                <p>Congratulations! You won!</p>
+                <button onClick={handleNextLevelClick}>Next Level</button>
+                <button onClick={handleTryAgainClick}>Try Again</button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     );
   };
 
-  return (
-    <div className="maze">
-      <div className="game-board">{createMaze()}</div>
-      {/* Use isMobile to conditionally render mobile-specific content */}
-      {isMobile ? createMaze() : null}
-    </div>
-  );
+  return <div className="Maze">{createMaze()}</div>;
 };
 
 export default Maze;
